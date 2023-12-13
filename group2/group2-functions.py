e2867511f0104384c12238e390f348130f14c41b
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-12-13 14:42:27 -0500

    adding group function assignments

---
+++ group2/group2-functions.py
@@ -0,0 +1,100 @@
+# The Quick Brown Fox
+'''
+Write a Python function that accepts a string and counts the number of upper and lower case letters.
+Sample String : 'The quick Brown Fox'
+Expected Output :
+No. of Upper case characters : 3
+No. of Lower case Characters : 13
+Your code should be simple, extremely readable, and well commented. Test thoroughly through every step. Be sure to use type hinting and proper documentation
+'''
+
+# Highest of our two numbers
+''' Write a function called get_highest that takes 2 numbers as parameters and returns the highest of the 2 numbers. 
+Your code should be simple, extremely readable, and well commented. Test thoroughly through every step. Be sure to use type hinting and proper documentation
+'''
+
+
+# New job at Chevy
+'''
+You have been hired by Chevy to look for non-chevy owners to send mailings to.  You have a simple list below of some drivers and the cars they own. Use Lambdas and the filter function to step through and solve this problem.
+
+cars = [{ "Driver": "Mike", "Car": "Honda"},
+        { "Driver": "Anthony", "Car": "Ford"},
+        { "Driver": "Reynelle", "Car": "Subaru"},
+        { "Driver": "Bri", "Car": "Subaru"},
+        { "Driver": "Julie", "Car": "Chevy"},
+        { "Driver": "Sam", "Car": "Chevy"},
+        { "Driver": "Judith", "Car": "Chevy"}]
+
+1. Write a function that will deliver true or false against a single dictionary in the list. After it is tested and you are sure the test results are correct write a lambda function.
+
+2. Write and test your lambda function based on the previously created function.
+
+3. Create a filter function based on your dataset and your lambda
+
+4. Deliver your data to management with just the driver's name (however you prefer)
+Your code should be simple, extremely readable, and well commented. Test thoroughly through every step. Be sure to use type hinting and proper documentation
+'''
+
+# Using Filter function lets grab everyone in this list who owns a chevy
+cars = [{ "Driver": "Mike", "Car": "Honda"},
+        { "Driver": "Anthony", "Car": "Ford"},
+        { "Driver": "Reynelle", "Car": "Subaru"},
+        { "Driver": "Bri", "Car": "Subaru"},
+        { "Driver": "Julie", "Car": "Chevy"},
+        { "Driver": "Sam", "Car": "Chevy"},
+        { "Driver": "Judith", "Car": "Chevy"}]
+
+
+# First create a function that delivers true or false depending on if the user owns a chevy
+
+# Next test that your function works on 1 dictionary in the list of cars
+
+# Create and test lambda from function
+
+# Use the filter function to deliver dictionaries with chevy owners
+
+# Loop through and deliver the name of the drivers in whatever format you prefer
+
+
+# Group 1 / Group 2
+'''
+Your development team is creating a large application. Group 1 is in charge of creating a function which will take a list of dictionaries.  From the data, please extract email addresses and deliver to Group 2 as a list which will be used for marketing.
+
+data = [
+	{
+		"name": "Fay Duke",
+		"email": "sed.nunc.est@protonmail.couk",
+		"phone": "1-240-443-8652"
+	},
+	{
+		"name": "Stuart Strong",
+		"email": "sit@protonmail.com",
+		"phone": "1-113-229-6241"
+	},
+	{
+		"name": "Adria Ortega",
+		"email": "ultricies.ornare@icloud.net",
+		"phone": "(425) 517-3787"
+	},
+	{
+		"name": "Angela Willis",
+		"email": "porttitor.vulputate@outlook.edu",
+		"phone": "1-847-221-3669"
+	},
+	{
+		"name": "Byron Guthrie",
+		"email": "aliquam.auctor@yahoo.ca",
+		"phone": "1-778-467-5941"
+	}
+]
+
+Group 2, you will create a function that takes in this list of valid email addresses. and return a list of only valid email addresses. Accepted top level domains (.com, .edu, .net) and other standard email format testing as needed. 
+'''
+
+
+
+
+
+
+
